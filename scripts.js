59b1a657b530a226076b7e9406aba9ce349cefbd
rvvn <pruthven@gmail.com>
2023-08-08 21:10:33 +0200

    load from json

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const guitarList = [
-        { id: 1, name: 'Guitar 1', image: 'images/guitar1.jpg', price: '$1000' },
-        { id: 2, name: 'Guitar 2', image: 'images/guitar2.jpg', price: '$1200' },
-        // ... add more guitars as needed
-    ];
+    fetch('guitars.json')
+        .then(response => response.json())
+        .then(guitarList => {
+            const guitarListContainer = document.getElementById('guitar-list');
 
-    const guitarListContainer = document.getElementById('guitar-list');
+            guitarList.forEach(guitar => {
+                const guitarCard = document.createElement('div');
+                guitarCard.className = 'guitar-card';
 
-    guitarList.forEach(guitar => {
-        const guitarCard = document.createElement('div');
-        guitarCard.className = 'guitar-card';
+                guitarCard.innerHTML = `
+                    <img src="${guitar.image}" alt="${guitar.name}">
+                    <h2>${guitar.name}</h2>
+                    <p>Price: ${guitar.price}</p>
+                    <button>Buy Now</button>
+                `;
 
-        guitarCard.innerHTML = `
-            <img src="${guitar.image}" alt="${guitar.name}">
-            <h2>${guitar.name}</h2>
-            <p>Price: ${guitar.price}</p>
-            <button>Buy Now</button>
-        `;
-
-        guitarListContainer.appendChild(guitarCard);
-    });
+                guitarListContainer.appendChild(guitarCard);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching guitars:", error);
+        });
 });
